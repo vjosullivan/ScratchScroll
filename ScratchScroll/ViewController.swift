685b270a3d957732fdd252c6660d422f17e3a9d2
Vince O'Sullivan (from home) <vjosullivan@gmail.com>
2017-04-20 07:45:25 +0100

    Initial Commit

---
+++ ScratchScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScratchScroll
+//
+//  Created by Vincent O'Sullivan on 20/04/2017.
+//  Copyright © 2017 Rose Cottage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
